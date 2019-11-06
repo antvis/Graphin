623c25834352533a0e4a818569786a4c296db662
pomelo-nwu <czynwu@outlook.com>
2019-11-06 21:57:48 +0800

    core:init the repo

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    extends: [
+        'airbnb',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+        'prettier/react',
+        'prettier/@typescript-eslint',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true, // Allows for the parsing of JSX
+        },
+    },
+    rules: {
+        'import/no-unresolved': 0,
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        'react/prop-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 0,
+        'react/display-name': 'off',
+        'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+        'react/jsx-props-no-spreading': 0,
+        'operator-assignment': 0,
+        'react/no-did-update-set-state': 0,
+        'consistent-return': 0,
+        'react/sort-comp': 0,
+        'no-plusplus': 'off', // i++
+        radix: 'off', // parseInt Missing radix parameter
+        'no-restricted-properties': 'off', // Math.pow(10, 2) === 10**2
+        'import/order': 0,
+    },
+};
