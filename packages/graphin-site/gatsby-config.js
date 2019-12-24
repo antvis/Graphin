0220fb3540b7655c82a37f59e66bcc48efc66f96
pomelo-nwu <czynwu@outlook.com>
2019-12-24 15:37:24 +0800

    :bug: move graphin to devDependencies

--- packages/graphin-site/gatsby-config.js
+++ packages/graphin-site/gatsby-config.js
@@ -113,9 +113,9 @@ module.exports = {
                 },
             },
         ],
-    },
-    docsearchOptions: {
-        apiKey: '159c16127929bd9fe6a3087f9ddddcb9',
-        indexName: 'antv_graphin',
+        docsearchOptions: {
+            apiKey: '159c16127929bd9fe6a3087f9ddddcb9',
+            indexName: 'antv_graphin',
+        },
     },
 };
