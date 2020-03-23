fa33d7039110eb0a566752d77a0354effdc1ddaf
pomelo-nwu <czynwu@outlook.com>
2020-03-23 22:13:52 +0800

    feat(site): add force demo

--- packages/graphin-site/examples/layout/force/demo/Force.jsx
+++ packages/graphin-site/examples/layout/force/demo/Force.jsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import Graphin, { Utils } from '@antv/graphin';
 import '@antv/graphin/dist/index.css'; // 引入Graphin CSS
 
-const data = Utils.mock(50)
-    .random(0.3)
-    .graphin();
+const data = Utils.mock(10)
+  .circle()
+  .graphin();
 
-const App = () => <Graphin data={data} layout={{ name: 'force' }} />;
+const App = () => <Graphin data={data} layout={{ name: 'force', options: { animation: true, enableWorker: true } }} />;
 
 ReactDOM.render(<App />, document.getElementById('container'));
