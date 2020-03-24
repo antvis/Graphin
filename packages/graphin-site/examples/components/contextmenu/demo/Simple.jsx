2557d6dbfed478212576f9ea5894f04b3e070cd0
pomelo-nwu <czynwu@outlook.com>
2020-03-24 20:16:08 +0800

    feat: Complete the demo of Toolbar and ContextMenu

---
+++ packages/graphin-site/examples/components/contextmenu/demo/Simple.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Graphin, { Utils } from '@antv/graphin';
+import { message } from 'antd';
+import '@antv/graphin/dist/index.css'; // 引入Graphin CSS
+import { ContextMenu } from '@antv/graphin-components';
+import '@antv/graphin-components/dist/index.css'; // Graphin 组件 CSS
+
+const data = Utils.mock(6)
+  .circle()
+  .graphin();
+
+const App = () => {
+  const options = [
+    {
+      key: 'deleteNode',
+      title: 'Delete',
+      visible: true,
+      iconType: 'delete',
+      onClick: e => {
+        const nodes = e.graph.findAllByState('node', 'selected');
+        const nodeIds = nodes.map(node => node.get('id'));
+        if (nodeIds.length === 0) {
+          message.info(`oh,你好像没有选中节点...`);
+        } else {
+          message.info(`确认删除节点 ${nodeIds.join(';')} 吗？`);
+        }
+      },
+    },
+    {
+      key: 'invertSelect',
+      title: 'Invert Select',
+      iconType: 'select',
+      visible: true,
+      onClick: () => {},
+    },
+  ];
+
+  return (
+    <div>
+      <Graphin data={data} layout={{ name: 'concentric' }}>
+        <ContextMenu options={options} />
+      </Graphin>
+    </div>
+  );
+};
+const rootElement = document.getElementById('container');
+ReactDOM.render(<App />, rootElement);
---
+++ packages/graphin-site/examples/components/toolbar/demo/Custom.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Graphin, { Utils } from '@antv/graphin';
+import '@antv/graphin/dist/index.css'; // 引入Graphin CSS
+import { Toolbar } from '@antv/graphin-components';
+import '@antv/graphin-components/dist/index.css'; // Graphin 组件 CSS
+
+const data = Utils.mock(10)
+  .circle()
+  .graphin();
+
+const App = () => {
+  const renderToolbar = (renderProps, _state) => {
+    const { toolbarCfg } = renderProps;
+    const items = [
+      {
+        id: 'custom',
+        name: 'custom',
+        icon: 'layout',
+        disabled: false,
+        action: () => {},
+        style: {},
+        renderTooltip: () => {
+          return <div>Custom</div>;
+        },
+      },
+    ];
+
+    return [...items, ...toolbarCfg];
+  };
+
+  return (
+    <div>
+      <Graphin data={data} layout={{ name: 'concentric' }}>
+        <Toolbar direction="vertical" render={renderToolbar} />
+      </Graphin>
+    </div>
+  );
+};
+const rootElement = document.getElementById('container');
+ReactDOM.render(<App />, rootElement);
