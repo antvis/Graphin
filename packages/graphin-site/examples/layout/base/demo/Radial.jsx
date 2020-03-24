4abf323646650cdbf2b461f6d0da0d7a557ee4ef
pomelo-nwu <czynwu@outlook.com>
2020-03-24 20:21:24 +0800

    feat: complete radial layout

--- packages/graphin-site/examples/layout/base/demo/Radial.jsx
+++ packages/graphin-site/examples/layout/base/demo/Radial.jsx
@@ -4,10 +4,26 @@ import ReactDOM from 'react-dom';
 import Graphin, { Utils } from '@antv/graphin';
 import '@antv/graphin/dist/index.css'; // 引入Graphin CSS
 
-const data = Utils.mock(10)
-  .circle()
+const data = Utils.mock(20)
+  .random()
   .graphin();
 
-const App = () => <Graphin data={data} layout={{ name: 'radial' }} />;
+const App = () => (
+  <Graphin
+    data={data}
+    layout={{
+      name: 'radial',
+      options: {
+        unitRadius: 100,
+        preventOverlap: true,
+        nodeSize: 40,
+        maxPreventOverlapIteration: 1000,
+        nodeSpacing: () => {
+          return 30;
+        },
+      },
+    }}
+  />
+);
 
 ReactDOM.render(<App />, document.getElementById('container'));
