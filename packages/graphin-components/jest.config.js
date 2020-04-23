4beabd56c79063f84b7063ea839f01685fbf7563
pomelo-nwu <czynwu@outlook.com>
2020-04-23 13:37:39 +0800

    fixci(common): Jest does not support ES module

--- packages/graphin-components/jest.config.js
+++ packages/graphin-components/jest.config.js
@@ -1,14 +1,15 @@
+const { jsWithTs: tsjPreset } = require('ts-jest/presets'); // eslint-disable-line @typescript-eslint/no-var-requires
+
 module.exports = {
-    preset: 'ts-jest',
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '/__tests__/.*\\.test.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFiles: ['jest-canvas-mock'],
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-    moduleNameMapper: {
-        '\\.(css|less|sass|scss)$': '<rootDir>/__mock__/styleMock.ts',
-    },
+  preset: 'ts-jest/presets/js-with-ts',
+  transform: tsjPreset.transform,
+  testRegex: '/__tests__/.*\\.test.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['jest-canvas-mock'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mock__/styleMock.ts',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
--- packages/graphin-components/src/components/ui/index.ts
+++ packages/graphin-components/src/components/ui/index.ts
@@ -1,15 +1,16 @@
-import Button from 'antd/es/button';
-import 'antd/es/button/style';
-import Tooltip from 'antd/es/tooltip';
-import 'antd/es/tooltip/style';
-import Icon from 'antd/es/icon';
-import 'antd/es/icon/style';
-import Popover from 'antd/es/popover';
-import 'antd/es/popover/style';
-import Progress from 'antd/es/progress';
-import 'antd/es/progress/style';
+// import Button from 'antd/es/button';
+// import 'antd/es/button/style';
+// import Tooltip from 'antd/es/tooltip';
+// import 'antd/es/tooltip/style';
+// import Icon from 'antd/es/icon';
+// import 'antd/es/icon/style';
+// import Popover from 'antd/es/popover';
+// import 'antd/es/popover/style';
+// import Progress from 'antd/es/progress';
+// import 'antd/es/progress/style';
+// import Menu from 'antd/es/menu';
+// import 'antd/es/menu/style';
 
-import Menu from 'antd/es/menu';
-import 'antd/es/menu/style';
+import { Button, Tooltip, Icon, Popover, Progress, Menu } from 'antd';
 
 export { Button, Tooltip, Icon, Popover, Progress, Menu };
