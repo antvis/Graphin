f6d3ffbc9ce07550ce3cabec4694f66e6d1953d2
萌萌的老胖子 <dongqi.ldq@alibaba-inc.com>
2020-07-15 15:03:48 +0800

    :bug: fix(common): fix typescript typing error

--- packages/graphin/src/events/change-zoom.ts
+++ packages/graphin/src/events/change-zoom.ts
@@ -19,25 +19,27 @@ const changeZoom = (graphin: Graphin) => {
 
   /** 缩放的时候隐藏IMAGE/TEXT */
   let timer: null | number = null;
-  graph!.on('viewportchange', (evt: ExtendedG6Event) => {
-    if (evt.action === 'zoom' && isZoomOptimize(graph, evt)) {
-      if (timer) window.clearTimeout(timer);
-      timer = window.setTimeout(() => {
-        try {
-          if (graphin.state.forceSimulation) {
-            /** 如果存在力导，那么缩放结束且力导未结束前，只展示keyShape */
-            if (graphin.state.forceSimulation.done) optimizeDrawing(graph, false);
-          }
-          /** 只有缩放比率大于keyShapeZoom，才展示所有的，否则只展示keyshape */
-          if (graph!.getZoom() > keyShapeZoom) {
+  if (graph) {
+    graph!.on('viewportchange', (evt: ExtendedG6Event) => {
+      if (evt.action === 'zoom' && isZoomOptimize(graph, evt)) {
+        if (timer) window.clearTimeout(timer);
+        timer = window.setTimeout(() => {
+          try {
+            if (graphin.state.forceSimulation) {
+              /** 如果存在力导，那么缩放结束且力导未结束前，只展示keyShape */
+              if (graphin.state.forceSimulation.done) optimizeDrawing(graph, false);
+            }
+            /** 只有缩放比率大于keyShapeZoom，才展示所有的，否则只展示keyshape */
+            if (graph!.getZoom() > keyShapeZoom) {
+              optimizeDrawing(graph, false);
+            }
+          } catch (error) {
             optimizeDrawing(graph, false);
           }
-        } catch (error) {
-          optimizeDrawing(graph, false);
-        }
-      }, 200);
-      optimizeDrawing(graph, true);
-    }
-  });
+        }, 200);
+        optimizeDrawing(graph, true);
+      }
+    });
+  }
 };
 export default changeZoom;
