9b8a8c1a0d96a5b92479875bde834b3de52181f9
聚则 <zhanning.bzn@antgroup.com>
2020-12-25 10:55:32 +0800

    feat: add graphin circle node docs

---
+++ packages/graphin/docs/render/element/graphin-circle.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Graphin, { Utils, Behaviors } from '@antv/graphin';
+
+const { ZoomCanvas } = Behaviors;
+
+const data = Utils.mock(10)
+  .circle()
+  .graphin();
+const layout = {
+  type: 'circular',
+};
+
+export default () => {
+  return (
+    <div>
+      <Graphin data={data} layout={layout}>
+        <ZoomCanvas disabled={true} />
+      </Graphin>
+    </div>
+  );
+};
