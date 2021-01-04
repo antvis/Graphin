40d886ed5109614febb81aad7aa2f2a87b8fc978
pomelo-nwu <czynwu@outlook.com>
2021-01-04 20:56:17 +0800

    feat(graphin):perfect docs

--- packages/graphin/src/behaviors/DragNode.tsx
+++ packages/graphin/src/behaviors/DragNode.tsx
@@ -2,18 +2,40 @@ import * as React from 'react';
 import useBehaviorHook from './useBehaviorHook';
 
 const defaultConfig = {
-  /** 是否禁用该功能 */
+  /**
+   * @description 是否禁用该功能
+   * @default false
+   */
   disabled: false,
-  /** 是否在拖拽节点时更新所有与之相连的边，默认为 true  */
+  /**
+   * @description 是否在拖拽节点时更新所有与之相连的边，默认为 true
+   * @default true
+   */
   updateEdge: true,
-  /** 节点拖拽时的绘图属性，默认为 { strokeOpacity: 0.6, fillOpacity: 0.6 } */
+  /**
+   * @description 节点拖拽时的绘图属性
+   * @default { strokeOpacity: 0.6, fillOpacity: 0.6 }
+   */
   delegateStyle: {},
-  // 是否开启delegate
+  /**
+   * @description 是否开启delegate
+   * @default false
+   */
   enableDelegate: false,
-  // 拖动节点过程中是否只改变 Combo 的大小，而不改变其结构
+  /**
+   * @description 拖动节点过程中是否只改变 Combo 的大小，而不改变其结构
+   * @default false
+   */
   onlyChangeComboSize: false,
-  // 拖动过程中目标 combo 状态样式
+  /**
+   * @description 拖动过程中目标 combo 状态样式
+   * @default ''
+   */
   comboActiveState: '',
+  /**
+   * @description 选中样式
+   * @default selected
+   */
   selectedState: 'selected',
 };
 
