f59da1c599b9785d4d221f8fdd1dba6a261c92bb
pomelo-nwu <czynwu@outlook.com>
2021-01-10 15:29:56 +0800

    feat(graphin):adjust export file

--- packages/graphin/src/index.ts
+++ packages/graphin/src/index.ts
@@ -1,34 +1,39 @@
 import Graphin from './Graphin';
-import GraphinContext from './GraphinContext';
+import GraphinContext, { GraphinContextType } from './GraphinContext';
 import Utils from './utils';
 import Layout from './layout';
-
 import Behaviors from './behaviors';
 import registerGraphinForce from './layout/inner/registerGraphinForce';
 import registerRenderLayout from './layout/inner/registerRenderLayout';
 import { registerGraphinCircle } from './shape';
 
-export {
-  /** G6类抛出去 */
-  default as G6,
-  /** 外部用户需要断言的 G6 类型 */
-  Graph,
-  IG6GraphEvent,
-} from '@antv/g6';
-
-export default Graphin;
-export { Utils, Layout, GraphinContext, Behaviors };
-
-export interface GraphEvent extends MouseEvent {
-  item: any;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  target: any;
-}
-
 /** 注册 Graphin force 布局 */
 registerGraphinForce();
 /** 注册 Graphin render 布局 */
 registerRenderLayout();
-
 /** 注册 Graphin Circle Node */
 registerGraphinCircle();
+
+/** export */
+export default Graphin;
+export { Utils, Layout, GraphinContext, Behaviors };
+/** export type */
+export { GraphinContextType };
+export {
+  /** export G6 */
+  default as G6,
+  /** export G6 Type  */
+  Graph,
+  IG6GraphEvent,
+  GraphData,
+  TreeGraphData,
+  NodeConfig,
+  EdgeConfig,
+} from '@antv/g6';
+
+export interface GraphEvent extends MouseEvent {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  item: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  target: any;
+}
