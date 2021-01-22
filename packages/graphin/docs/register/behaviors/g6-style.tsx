ad46384bc22d0880a7d847893f8d9457b262e420
pomelo-nwu <czynwu@outlook.com>
2021-01-22 16:55:54 +0800

    docs(graphin):add behaviors demos

---
+++ packages/graphin/docs/register/behaviors/g6-style.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Graphin, { IG6GraphEvent, Utils, GraphinData } from '@antv/graphin';
+import { INode, NodeConfig } from '@antv/g6';
+
+const data: GraphinData = Utils.mock(8)
+  .circle()
+  .graphin();
+
+Graphin.registerBehavior('sampleBehavior', {
+  getEvents() {
+    return {
+      'node:click': 'onClick',
+    };
+  },
+  onClick(evt: IG6GraphEvent) {
+    const node = evt.item as INode;
+    const model = node.getModel() as NodeConfig;
+    console.log(model);
+    // TODO
+  },
+});
+
+/**
+ * @example
+ * https://g6.antv.vision/zh/docs/api/Behavior
+ */
+export default () => {
+  return <Graphin data={data} layout={{ type: 'concentric' }} modes={{ default: ['sampleBehavior'] }} />;
+};
---
+++ packages/graphin/docs/register/behaviors/index.md
@@ -0,0 +1,19 @@
+---
+title: 注册Behaviors
+order: 0
+group:
+  path: /register
+  title: 注册机制
+nav:
+  title: Graphin
+  path: /graphin
+  order: 5
+---
+
+## 【推荐】 Graphin 注册 Behavior
+
+<code src='./graphin-style.tsx'>
+
+## 【兼容】 G6 注册 Behavior 的写法
+
+<code src='./g6-style.tsx'>
