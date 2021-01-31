a5e139a3a5c71444922aca38c9a43aa7237c5ffc
pomelo-nwu <czynwu@outlook.com>
2021-01-31 19:53:27 +0800

    feat:add background for line and fix typing error

--- packages/graphin/src/shape/graphin-line.ts
+++ packages/graphin/src/shape/graphin-line.ts
@@ -1,6 +1,6 @@
-// @ts-nocheck
-import { Group } from '@antv/g-canvas';
-import G6, { EdgeConfig } from '@antv/g6';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IGroup } from '@antv/g-canvas';
+import G6, { EdgeConfig, IEdge, INode, ModelConfig } from '@antv/g6';
 
 import { deepMix } from '@antv/util';
 import { EdgeStyle } from '../typings/type';
@@ -13,7 +13,7 @@ export enum EnumNodeAndEdgeStatus {
   DISABLED = 'disabled',
 }
 
-export function removeDumpAttrs(attrs) {
+export function removeDumpAttrs(attrs: any) {
   Object.keys(attrs).forEach((key) => {
     if (attrs[key] === undefined) {
       delete attrs[key];
@@ -21,19 +21,17 @@ export function removeDumpAttrs(attrs) {
   });
   return attrs;
 }
-export function parseLabel(json) {
+export function parseLabel(json: EdgeStyle['label']) {
   const { value, ...others } = json;
   const attrs = {
     id: 'label',
-    x: 0,
-    y: 0,
     text: value,
     ...others,
   };
   return removeDumpAttrs(attrs);
 }
 
-export function parseKeyShape(json) {
+export function parseKeyShape(json: EdgeStyle['keyshape']) {
   const { ...others } = json;
   const attrs = {
     id: 'keyshape',
@@ -42,7 +40,7 @@ export function parseKeyShape(json) {
   return removeDumpAttrs(attrs);
 }
 
-export function parseHalo(json) {
+export function parseHalo(json: EdgeStyle['halo']) {
   const { ...others } = json;
   const attrs = {
     id: 'halo',
@@ -51,107 +49,154 @@ export function parseHalo(json) {
   return removeDumpAttrs(attrs);
 }
 
-const parseAttr = (style, itemShapeName: string) => {
-  const itemStyle = style[itemShapeName] || {};
+const parseAttr = (style: EdgeStyle, itemShapeName: string) => {
   if (itemShapeName === 'keyshape') {
-    return parseKeyShape(itemStyle);
+    return parseKeyShape(style.keyshape || {});
   }
   if (itemShapeName === 'halo') {
-    return parseHalo(itemStyle);
+    return parseHalo(style.halo || {});
   }
   if (itemShapeName === 'label') {
-    return parseLabel(itemStyle);
+    return parseLabel(style.label || {});
   }
-  return itemStyle;
+  return {};
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default () => {
   G6.registerEdge('graphin-line', {
-    draw(cfg: EdgeConfig, group: Group) {
-      try {
-        const style = deepMix({}, cfg.style) as EdgeStyle;
-        /** 将初始化样式存储在model中 */
-
+    draw(cfg: ModelConfig | undefined, group: IGroup | undefined) {
+      const style = deepMix({}, cfg && cfg.style) as EdgeStyle;
+      /** 将初始化样式存储在model中 */
+      if (cfg) {
         // eslint-disable-next-line no-underscore-dangle
         cfg._initialStyle = { ...style };
+      }
 
-        const { label, halo, keyshape: keyShapeStyle } = style;
+      const { startPoint = { x: 0, y: 0 }, endPoint = { x: 0, y: 0 } } = cfg as EdgeConfig;
 
-        const hasLabel = label.value;
-        const { startPoint, endPoint } = cfg;
+      const { label, halo, keyshape: keyShapeStyle } = style;
 
-        const d = (keyShapeStyle?.lineWidth || 1) + 1;
-        // TODO:支持多边
-        const path = [
-          ['M', startPoint.x, startPoint.y],
-          ['L', endPoint.x, endPoint.y],
-        ];
+      const hasLabel = label.value;
 
-        /** 光环 */
-        group.addShape('path', {
-          attrs: {
-            id: 'halo',
-            path,
-            lineWidth: keyShapeStyle.lineWidth + 10,
-            ...halo,
+      const lineWidth = keyShapeStyle?.lineWidth || 1;
+      const d = lineWidth + 5;
+
+      // TODO:支持多边
+      const path = [
+        ['M', startPoint.x, startPoint.y],
+        ['L', endPoint.x, endPoint.y],
+      ];
+
+      /** 光环 */
+      (group as IGroup).addShape('path', {
+        attrs: {
+          id: 'halo',
+          path,
+          lineWidth: lineWidth + 10,
+          stroke: halo.stroke || keyShapeStyle.stroke,
+          strokeOpacity: halo.strokeOpacity || 0.4,
+          ...halo,
+        },
+        draggable: true,
+        name: 'halo',
+        visible: halo.visible !== false,
+      });
+
+      /** 主路径 */
+      const key = (group as IGroup).addShape('path', {
+        attrs: {
+          id: 'keyshape',
+          path,
+          endArrow: {
+            d: 0,
+            path: `M 0,0 L ${d},${d / 2} L ${d},-${d / 2} Z`,
+            fill: keyShapeStyle.stroke,
           },
-          draggable: true,
-          name: 'halo',
-          visible: false,
-        });
+          ...keyShapeStyle,
+        },
+        draggable: true,
+        name: 'keyshape',
+      });
 
-        /** 主路径 */
-        const key = group.addShape('path', {
-          attrs: {
-            id: 'keyshape',
-            path,
-            endArrow: {
-              d: -d / 2,
-              path: `M 0,0 L ${d},${d / 2} L ${d},-${d / 2} Z`,
-            },
-            ...keyShapeStyle,
-          },
-          draggable: true,
-          name: 'keyshape',
-        });
+      /** 标签 */
+      if (hasLabel) {
+        const { value, fontSize = 8, offset = [0, 0], background, ...others } = label;
+        const hasBackground = Boolean(background);
+        const [offsetX, offsetY] = offset;
+        /** 设置标签的背景 */
+        if (hasBackground) {
+          const calcWidth = String(value).length * fontSize * 0.6;
+          const calcHeight = fontSize * 1.8;
+          const defaultBackground = {
+            fill: '#fff',
+            width: calcWidth,
+            height: calcHeight,
+            stroke: keyShapeStyle.stroke,
+            lineWidth: 1,
+            radius: 6,
+          };
 
-        /** 标签 */
-        if (hasLabel) {
-          const { value, ...others } = label;
-          const labelShape = group.addShape('text', {
+          const { fill, width, height, stroke, ...otherBackgroundAttrs } = { ...defaultBackground, ...background };
+          const labelBackgroundShape = (group as IGroup).addShape('rect', {
             attrs: {
-              id: 'label',
-              x: 0,
-              y: 0,
-              text: value,
-              ...others,
+              id: 'label-background',
+              x: -width / 2,
+              y: -height / 2,
+              width,
+              height,
+              fill,
+              stroke,
+              ...otherBackgroundAttrs,
             },
             draggable: true,
-            name: 'label',
+            name: 'label-background',
           });
+
           /** 处理标签自动旋转问题 */
-          labelShape.rotate(
+          labelBackgroundShape.rotate(
             endPoint.x - startPoint.x === 0
               ? Math.PI / 2
               : Math.atan((endPoint.y - startPoint.y) / (endPoint.x - startPoint.x)),
           );
-          labelShape.translate((startPoint.x + endPoint.x) / 2, (startPoint.y + endPoint.y) / 2);
+          labelBackgroundShape.translate((startPoint.x + endPoint.x) / 2, (startPoint.y + endPoint.y) / 2);
         }
-        return key;
-      } catch (error) {
-        console.error(error);
+        /** 设置标签的文本 */
+        const labelShape = (group as IGroup).addShape('text', {
+          attrs: {
+            id: 'label',
+            x: offsetX,
+            y: hasBackground ? offsetY + fontSize / 2 : offsetY - fontSize / 2,
+            text: value,
+            fontSize,
+            ...others,
+          },
+          draggable: true,
+          name: 'label',
+        });
+        /** 处理标签自动旋转问题 */
+        labelShape.rotate(
+          endPoint.x - startPoint.x === 0
+            ? Math.PI / 2
+            : Math.atan((endPoint.y - startPoint.y) / (endPoint.x - startPoint.x)),
+        );
+        labelShape.translate((startPoint.x + endPoint.x) / 2, (startPoint.y + endPoint.y) / 2);
       }
+      return key;
     },
-    setState(name: EnumNodeAndEdgeStatus, value: string, item: EdgeConfig) {
+    setState(
+      name: EnumNodeAndEdgeStatus | string | undefined,
+      value: string | boolean | undefined,
+      item: IEdge | INode | undefined,
+    ) {
       if (!name) return;
-      const model = item.getModel() as EdgeConfig;
-      const shapes = item.getContainer().get('children'); // 顺序根据 draw 时确定
-      const initStateStyle = deepMix({}, model.style.status);
+      const model = (item as IEdge).getModel() as EdgeConfig;
+      const shapes = (item as IEdge).getContainer().get('children'); // 顺序根据 draw 时确定
+      const initStateStyle = deepMix({}, (model.style as EdgeStyle).status);
 
-      const initialStyle = item.getModel()._initialStyle as EdgeStyle;
+      const initialStyle = (item as IEdge).getModel()._initialStyle as EdgeStyle;
 
-      const status = item._cfg?.states || [];
+      const status = (item as IEdge)._cfg?.states || [];
 
       try {
         Object.keys(initStateStyle).forEach((statusKey) => {
