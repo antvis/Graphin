36ab20a42cd7a71b8ad149536fd675f07d98e685
pomelo-nwu <czynwu@outlook.com>
2021-03-08 19:23:42 +0800

    docs: addNode in GraphSchema

--- packages/graphin/docs/solution/graph-database/graph-schema/index.tsx
+++ packages/graphin/docs/solution/graph-database/graph-schema/index.tsx
@@ -1,39 +1,20 @@
 import React from 'react';
 import Graphin, { Utils } from '@antv/graphin';
 import { Toolbar, ContextMenu } from '@antv/graphin-components';
-import { PlusOutlined, SwapOutlined } from '@ant-design/icons';
+import { PlusOutlined } from '@ant-design/icons';
 import AntdMenu from './AntdMenu';
+
+const { Menu } = ContextMenu;
 
 const source = Utils.mock(6).circle().graphin();
 source.edges = [];
 
-const options = [
-  {
-    key: 'addEdge',
-    name: (
-      <span>
-        添加关系 <SwapOutlined />
-      </span>
-    ),
-  },
-  {
-    key: 'addNode',
-    name: (
-      <span>
-        添加实体 <PlusOutlined />
-      </span>
-    ),
-  },
-];
-
+let index = 5;
 const SchemaBuilder = () => {
   const [state, setState] = React.useState({
     data: source,
     layout: 'circular',
   });
-  const handleChange = (graphin, option) => {
-    console.log('option', option);
-  };
 
   const { data, layout } = state;
 
@@ -48,17 +29,50 @@ const SchemaBuilder = () => {
     });
   };
 
+  const handleAddNode = item => {
+    console.log(item);
+    const { key } = item;
+    if (key === 'add-node') {
+      index = index + 1;
+      setState({
+        data: {
+          ...data,
+          nodes: [
+            ...data.nodes,
+            {
+              id: `node-${index}`,
+              style: {
+                label: {
+                  value: `node-${index}`,
+                },
+              },
+            },
+          ],
+        },
+        layout: 'circular',
+      });
+    }
+  };
+  console.log(data);
+
   return (
     <div>
       <Graphin data={data} layout={{ type: layout }}>
-        <Toolbar
-          style={{ position: 'absolute', left: 0, right: 'unset' }}
-          options={options}
-          onChange={handleChange}
-          direction="horizontal"
-        />
         <ContextMenu bindType="node" style={{ width: '140px' }}>
           <AntdMenu handleAddEdge={handleAddEdge} />
+        </ContextMenu>
+        <ContextMenu bindType="canvas" style={{ width: '140px' }}>
+          <Menu
+            bindType="canvas"
+            options={[
+              {
+                icon: <PlusOutlined />,
+                key: 'add-node',
+                name: '添加节点',
+              },
+            ]}
+            onChange={handleAddNode}
+          />
         </ContextMenu>
       </Graphin>
     </div>
