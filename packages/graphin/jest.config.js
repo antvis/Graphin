4b329f4c5cec12858eb5a6fa195c8f46bcb63d6d
clifford <cfajardo@linkedin.com>
2021-12-04 18:40:11 -0800

    chore: upgrade tesing devDependencies

--- packages/graphin/jest.config.js
+++ packages/graphin/jest.config.js
@@ -1,16 +1,14 @@
-const { jsWithTs: tsjPreset } = require('ts-jest/presets'); // eslint-disable-line
-
 module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  // transform: {
-  //     '^.+\\.tsx?$': 'ts-jest',
-  // },
-  transform: tsjPreset.transform,
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   testRegex: '/__tests__/.*\\.test.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   //   setupFiles: ['jest-canvas-mock'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mock__/styleMock.ts',
   },
