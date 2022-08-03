d237b2106a2ebb2821233a376ed81fb0688fc8b3
pomelo-nwu <czynwu@outlook.com>
2022-08-03 10:19:54 +0800

    feat: update G6

--- .umirc.js
+++ .umirc.js
@@ -92,8 +92,8 @@ export default {
     'https://gw.alipayobjects.com/os/lib/react-dom/16.13.1/umd/react-dom.development.js',
     'https://gw.alipayobjects.com/os/lib/antd/4.6.6/dist/antd-with-locales.js',
     /** G6 **/
-    'https://gw.alipayobjects.com/os/lib/antv/g6/4.6.4/dist/g6.min.js',
-    'https://gw.alipayobjects.com/os/lib/antv/g6/4.6.4/dist/g6.min.js.map',
+    'https://gw.alipayobjects.com/os/lib/antv/g6/4.6.17/dist/g6.min.js',
+    'https://gw.alipayobjects.com/os/lib/antv/g6/4.6.17/dist/g6.min.js.map',
     /** lodash */
     'https://gw.alipayobjects.com/os/lib/lodash/4.17.20/lodash.min.js',
   ],
